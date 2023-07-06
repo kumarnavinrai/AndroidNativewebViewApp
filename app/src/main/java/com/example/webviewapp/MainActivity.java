6d69cdb46ab81fad53a2818686af833432d6aca6
Navin Kumar <kumarnavinrai@hotmail.com>
2023-07-06 19:44:30 +0800

    first commit

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.webviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    ProgressBar pgBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pgBar = findViewById(R.id.pgBar);
+        webView = findViewById(R.id.webView);
+
+        webView.loadUrl("https://www.google.com");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                pgBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                pgBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
